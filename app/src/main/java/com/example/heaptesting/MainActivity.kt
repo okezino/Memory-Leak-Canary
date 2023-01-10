724ce7610eb8980f05c10605a527c131cc8f53c1
okezino <okezi002@gmail.com>
2023-01-10 13:15:15 +0100

    static context memory leak

--- app/src/main/java/com/example/heaptesting/MainActivity.kt
+++ app/src/main/java/com/example/heaptesting/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.heaptesting
 
 import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var text : TextView
-    lateinit var textLarge : TextView
+    lateinit var button: Button
+    companion object{
+        lateinit var context : Context
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val am: ActivityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
-
-        text = findViewById(R.id.single_text)
-        textLarge = findViewById(R.id.single_large_text)
-
-        text.setText(am.memoryClass.toString())
-        textLarge.setText(am.largeMemoryClass.toString())
-
+        button = findViewById(R.id.next)
+        activateClickListener()
     }
+
+
+    private fun navigateToSecondScreen(){
+        Intent(this, SecondActivity::class.java).also {
+            startActivity(it)
+        }
+    }
+
+
+    private fun activateClickListener(){
+        button.setOnClickListener {
+            navigateToSecondScreen()
+        }
+    }
+
+
+
 }
